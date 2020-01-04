231366de6db3ee4146441ef790c1688d068192cb
Bob Wakefield <bob@cockleburr.net>
2020-01-04 10:37:12 -0600

    Initial Commit

---
+++ TodayWidgetDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayWidgetDemo
+//
+//  Created by Bob Wakefield on 1/4/20.
+//  Copyright © 2020 Bob Wakefield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
